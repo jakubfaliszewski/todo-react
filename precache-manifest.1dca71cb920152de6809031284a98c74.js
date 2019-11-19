ce225caa0c085cf6bc47a90ba294193b803d3f4e
jakubfaliszewski <jfaliszewski@interia.pl>
2019-11-19 16:18:41 +0100

    Updates

---
+++ precache-manifest.1dca71cb920152de6809031284a98c74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5782ab7cc37b3cc220d9b243f4698626",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6ce5a30642d2eb25283c",
+    "url": "/todo-react/static/css/main.730991eb.chunk.css"
+  },
+  {
+    "revision": "82d7f0e39bfb9aa0ea76",
+    "url": "/todo-react/static/js/2.f0c8bdaa.chunk.js"
+  },
+  {
+    "revision": "6ce5a30642d2eb25283c",
+    "url": "/todo-react/static/js/main.a79abfd7.chunk.js"
+  },
+  {
+    "revision": "c6797c52c56e6c11c6e7",
+    "url": "/todo-react/static/js/runtime-main.9f4ee3a5.js"
+  }
+]);
